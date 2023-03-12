6027a698b2ed8f9539ef16ff3a426b86a248686d
Mohammad Yudha Pamungkas <yudhapamungkas0300@gmail.com>
2023-03-12 18:41:16 +0700

    WPU Laravel 8 Eps 15 - User Registration

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,6 +2,17 @@
 @section('container')
     <div class="row justify-content-center">
         <div class="col-md-5">
+
+            @if (session()->has('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+
+                    </button>
+                </div>
+            @endif
+
+
             <main class="form-signin">
                 <h1 class="h3 mb-3 fw-normal">Login Page</h1>
                 <form>
